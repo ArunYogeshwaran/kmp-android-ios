ae8b49f26c863c476e7e3142c48a22fd398e933c
Arun <arun.yogeshwaran@zenjob.com>
2023-08-04 23:41:11 +0530

    Adding Kamel for network image loading and ktor for network cross-platform network calls

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,13 +33,15 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation("media.kamel:kamel-image:0.7.1")
+                implementation("io.ktor:ktor-client-core:2.3.3")
             }
         }
         val androidMain by getting {
             dependencies {
-                api("androidx.activity:activity-compose:1.6.1")
+                api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.9.0")
+                api("androidx.core:core-ktx:1.10.1")
             }
         }
         val iosX64Main by getting
@@ -50,6 +52,10 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation ("io.ktor:ktor-client-darwin:2.3.3")
+            }
         }
     }
 }
